9bae2f8325c70efa83fe1e9d135efd0b770ac595
michellejt <mtongas@gmail.com>
2023-02-16 00:22:37 +1030

    add lazy loading

---
+++ assets/js/custom.js
@@ -0,0 +1,15 @@
+const loadImages = document.querySelectorAll('img.lazy')
+
+const observer = new IntersectionObserver(observerHandler, {
+    threshold: 1.0
+})
+
+function observerHandler(entries, observer) {
+  entries.forEach((entry) => {
+     if (entry.intersectionRatio > 0) {
+       entry.target.src = entry.target.dataset.src
+       entry.target.classList.remove('lazy')
+     } 
+  });
+}
+loadImages.forEach(img => observer.observe(img))
