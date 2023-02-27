d5d1f34638893559bcd5665482573195bf683b4e
michellejt <mtongas@gmail.com>
2023-02-28 00:36:26 +1030

    js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,32 @@
-console.log("hallp")
-const loadImages = document.querySelectorAll('img.halp')
+const observerOptions = {
+  root: null,
+  threshold: 0,
+  rootMargin: '0px 0px -50px 0px'
+};
 
-const observer = new IntersectionObserver(observerHandler, {
-    threshold: 0.1
-})
-
-function observerHandler(entries, observer) {
-  entries.forEach((entry) => {
-     if (entry.intersectionRatio > 0) {
-       entry.target.src = entry.target.dataset.src
-      // entry.target.classList.remove('lazy')
-     } 
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+      entry.target.classList.toggle("in-view",entry.isIntersecting) 
   });
+}, observerOptions);
+
+window.addEventListener('DOMContentLoaded', (event) => { 
+  const articles =Array.from(document.querySelectorAll('fadein')); 
+  for (let article of articles) {
+    observer.observe(article);
+  }   
+});
+
+const typing = document.querySelector('#typing')
+let i = 0;
+let text = "A short comic about life and videogames.";
+let speed = 100;
+
+function typeWriter() {
+ if(i < text.length) {
+     typing.innerHTML += text.charAt(i);
+     i++;
+     setTimeout(typeWriter, speed);
+ }
 }
-loadImages.forEach(img => observer.observe(img))
+setTimeout(typeWriter,2500);
